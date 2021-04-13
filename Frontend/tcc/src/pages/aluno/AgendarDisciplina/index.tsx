b2d6f12ac55367b7405bf81e014f156e54478815
GabrielDEV <gabrielfernella07@hotmail.com>
2021-04-12 23:12:06 -0300

    update

--- Frontend/tcc/src/pages/aluno/AgendarDisciplina/index.tsx
+++ Frontend/tcc/src/pages/aluno/AgendarDisciplina/index.tsx
@@ -3,6 +3,7 @@ import PageHeader from '../../../components/PageHeader';
 import backgroundImg from '../../../assets/images/success-background.svg';
 
 import './styles.scss';
+import Input from '../../../components/Input';
 
 function ListDisciplina() {
   function select() {
@@ -22,45 +23,72 @@ function ListDisciplina() {
       </PageHeader>
 
       <main>
-        <fieldset>
-          <div id="disciplina">
-            <h2>Computação em Nuvem</h2>
+        <form onSubmit={ListDisciplina}>
+          <fieldset>
+            <div id="disciplina">
+              <h2>Computação em Nuvem</h2>
 
-            <h3> + Gabriel Oliveira Santos</h3>
+              <h3> + Gabriel Oliveira Santos</h3>
 
-            <div>
-              <h4>Tags:</h4>
-              <p>tags</p>
+              <div>
+                <h4>Tags:</h4>
+                <p>tags</p>
+              </div>
+
+              <h4>Descrição:</h4>
+              <p id="desc">
+                Nós aproveitamos o poder da mudança para criar novo e
+                extraordinário valor, colocando cloud no coração dos seus
+                negócios. Nossa abordagem põe as necessidades da sua empresa em
+                primeiro lugar. Desenvolvemos soluções específicas de indústria
+                para que você migre para a nuvem e usufrua dela agora.
+              </p>
+
+              <div>
+                <h4>Valor: R$ 30 /hora</h4>
+              </div>
             </div>
+            <div className="agendar">
+              <h3>Disponibilidades:</h3>
+              <div id="dia-disponibilidade">
+                <div>
+                  <h4>Segunda</h4>
+                  <p>das 14h até 19h</p>
+                </div>
+                <div>
+                  <h4>Terça</h4>
+                  <p>das 14h até 19h</p>
+                </div>
+              </div>
+              <div id="btn-agendar">
+                <Input
+                  name="entrada"
+                  label="Das"
+                  type="time"
+                  value="value"
+                  required
+                />
+                <Input
+                  name="saida"
+                  label="Até"
+                  type="time"
+                  value="value"
+                  required
+                />
+                <button type="submit" onClick={select}>
+                  Agendar
+                </button>
+              </div>
+            </div>
+          </fieldset>
 
-            <h4>Descrição:</h4>
-            <p id="desc">
-              Nós aproveitamos o poder da mudança para criar novo e
-              extraordinário valor, colocando cloud no coração dos seus
-              negócios. Nossa abordagem põe as necessidades da sua empresa em
-              primeiro lugar. Desenvolvemos soluções específicas de indústria
-              para que você migre para a nuvem e usufrua dela agora.
+          <footer>
+            <p>
+              Selecione uma das disciplinas e veja a disponibilidade para
+              agendamento!
             </p>
-
-            <h4>Disponibilidades:</h4>
-            <p>Segunda-feira, Terça-feira, Sexta-feira, Sabado, Domingo</p>
-
-            <div>
-              <h4>Valor: R$ 30 /hora</h4>
-            </div>
-
-            <button type="button" onClick={select}>
-              Ver disponibilidade
-            </button>
-          </div>
-        </fieldset>
-
-        <footer>
-          <p>
-            Selecione uma das disciplinas e veja a disponibilidade para
-            agendamento!
-          </p>
-        </footer>
+          </footer>
+        </form>
       </main>
     </div>
   );
