e9a732a3a4cd091651370f2527ea7becfe0d31e3
Gabriel Tadachi <tadabiel2013@gmail.com>
2021-10-27 22:32:36 -0300

    Removendo Projetos Antigos

--- v1/back/tcc/src/modules/aluno/repositories/IAlunoTokensRepository.ts
+++
@@ -1,6 +0,0 @@
-import AlunoToken from '../infra/typeorm/entities/AlunoToken';
-
-export default interface IAlunoTokensRepository {
-  generate(user_id: string): Promise<AlunoToken>;
-  findByToken(token: string): Promise<AlunoToken | undefined>;
-}
--- v1/back/tcc/src/modules/professor/repositories/IProfessorTokensRepository.ts
+++
@@ -1,6 +0,0 @@
-import TeacherToken from '../infra/typeorm/entities/ProfessorToken';
-
-export default interface IProfessorTokensRepository {
-  generate(user_id: string): Promise<TeacherToken>;
-  findByToken(token: string): Promise<TeacherToken | undefined>;
-}
